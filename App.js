d07908cbbba672e429d90a23fa983b55c3673818
onurerentasci <onurerentasci@gmail.com>
2023-07-22 23:15:56 +0300

    react navigation added and set

--- App.js
+++ App.js
@@ -1,9 +1,57 @@
 import React from "react";
 import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Ionicons } from "@expo/vector-icons";
+
+import ChatList from "./screens/ChatList";
+import Chat from "./screens/Chat";
+import SignUp from "./screens/SignUp";
+import Signin from "./screens/Signin";
+import Settings from "./screens/Settings";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Stack = createNativeStackNavigator();
+
+const Tabs = createBottomTabNavigator();
+
+const TabsNavigator = () => {
+  return (
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          return (
+            <Ionicons
+              name={route.name === "chatList" ? "chatbubbles" : "settings"}
+              color={color}
+              size={size}
+            />
+          );
+        },
+      })}
+    >
+      <Tabs.Screen name="chatList" component={ChatList} />
+      <Tabs.Screen name="settings" component={Settings} />
+    </Tabs.Navigator>
+  );
+};
 
 const App = () => {
-  return <NavigationContainer></NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={TabsNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Chat" component={Chat}/>
+        <Stack.Screen name="Signin" component={Signin}/>
+        <Stack.Screen name="Signup" component={SignUp}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ screens/SignIn.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Signin = () => {
+  return <Text>Hi, Signin!</Text>;
+};
+
+export default Signin;
