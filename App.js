3484f5cfd998192552d866c72065bb16c2f6ed7e
onurerentasci <onurerentasci@gmail.com>
2023-07-22 22:45:41 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+const App = () => {
+  return <NavigationContainer></NavigationContainer>;
+};
+
+export default App;
